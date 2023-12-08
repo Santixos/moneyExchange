269e4eb6a8d5774fa544570a92e52326e19d7550
Santino Sottile <santinosottile80@gmail.com>
2023-12-08 20:34:05 +0100

    swap button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_one = document.getElementById('currency-one'); /*document.querySelector('#currency-one') è equivalente*/
 const amountEl_one = document.getElementById('amount-one');
 const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
